ab3d5d478564a4ea88462f069fd09212d7e2cfe7
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-01 19:40:51 -0300

    feat: create sections for test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,43 @@
+import Navigation from '../components/navigation';
+
 const Index = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-3xl">Home Page</h1>
-      </div>
-    </div>
+    <>
+      <Navigation />
+      <main>
+        <section id="home" className="pt-20 min-h-screen bg-black text-white">
+          <h2 className="text-4xl">Início</h2>
+        </section>
+
+        <section
+          id="about"
+          className="pt-20 min-h-screen bg-gray-900 text-white"
+        >
+          <h2 className="text-4xl">Sobre</h2>
+        </section>
+
+        <section
+          id="skills"
+          className="pt-20 min-h-screen bg-gray-800 text-white"
+        >
+          <h2 className="text-4xl">Habilidades</h2>
+        </section>
+
+        <section
+          id="projects"
+          className="pt-20 min-h-screen bg-gray-700 text-white"
+        >
+          <h2 className="text-4xl">Projetos</h2>
+        </section>
+
+        <section
+          id="contact"
+          className="pt-20 min-h-screen bg-gray-600 text-white"
+        >
+          <h2 className="text-4xl">Contato</h2>
+        </section>
+      </main>
+    </>
   );
 };
 
